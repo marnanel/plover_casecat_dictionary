1be9850284ef0121e6c52dc3e0dd7d7c38693e50
Thomas Thurman <thomas@thurman.org.uk>
2018-02-19 00:41:11 +0000

    woohoo, it passes. Now to write all the rest of the tests

--- test/test_casecat_dict.py
+++ test/test_casecat_dict.py
@@ -7,30 +7,65 @@
 import os
 import unittest
 
-from plover.dictionary.casecat_dict import CaseCatDictionary
+from plover_casecat_dictionary import CaseCatDictionary
 
-class CaseCatDictionaryTestCase(unittest.TestCase):
+from plover import log, system
+from plover.config import DEFAULT_SYSTEM_NAME
+from plover.dictionary.base import load_dictionary
+from plover.formatting import Formatter
+from plover.registry import registry
+from plover.steno import normalize_steno, Stroke
+from plover.steno_dictionary import StenoDictionary
+from plover.translation import Translator
+
+log.set_level(log.DEBUG)
+
+DICTIONARIES = {
+        u'S-abc.sgdct': {
+            'mappings': {'S': u'abc'},
+            },
+        u'S-abc,T-abc.sgdct': {
+            'mappings': {'S': u'abc', 'T': u'abc'},
+            },
+        u'S-dis.sgdct': {
+            'mappings': {'S': u'{dis^}'},
+            },
+        u'S-ing.sgdct': {
+            'mappings': {'S': u'{^ing}'},
+            },
+        u'S_S-abc.sgdct': {
+            'mappings': {'S/S': u'abc'},
+            },
+
+        # No test for "cap up", because
+        # we don't yet have a genuine file
+        # that contains it
+
+        }
+
+class CaseCatDictionaryTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        registry.update()
+        system.setup(DEFAULT_SYSTEM_NAME)
+
+    def _dictionaries_iterator(self):
+        for (filename, expected) in sorted(DICTIONARIES.items()):
+
+            dic = load_dictionary(os.path.join(os.path.dirname(__file__), filename))
+            yield (filename, expected, dic)
+
+    def test_readonly(self):
+
+        for (filename, expected, dic) in self._dictionaries_iterator():
+            self.assertTrue(dic.readonly)
 
     def test_load_dictionary(self):
 
-        for filename, expected in (
+        for (filename, expected, dic) in self._dictionaries_iterator():
 
-                (u'S-abc.sgdct',
-                    {('S',): u'abc'}),
-                (u'S-abc,T-abc.sgdct',
-                    {('S',): u'abc', ('T',): u'abc'}),
-                (u'S-dis.sgdct',
-                    {('S',): u'{dis^}'}),
-                (u'S-ing.sgdct',
-                    {('S',): u'{^ing}'}),
-                (u'S_S-abc.sgdct',
-                    {('S','S'): u'abc'}),
+            for (k, v) in expected['mappings'].items():
+                self.assertEqual(dic[k], v)
 
-                # No test for "cap up", because
-                # we don't yet have a genuine file
-                # that contains it
 
-                ):
-
-            d = CaseCatDictionary.load(os.path.join('test', filename))
-            self.assertEqual(dict(d.items()), expected)
