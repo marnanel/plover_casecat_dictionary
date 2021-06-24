4ff5493f0ed8b3fc272778aa2bc8ba48d4bffb49
Benoit Pierre <benoit.pierre@gmail.com>
2021-06-24 02:36:09 +0200

    use Plover 4.0.0.dev10 torture tests

--- test/test_casecat_dict.py
+++ test/test_casecat_dict.py
@@ -1,71 +1,50 @@
-# -*- coding: utf-8 -*-
-# Copyright (c) 2013 Hesky Fisher
-# See LICENSE.txt for details.
-
 """Unit tests for CaseCat."""
 
-import os
-import unittest
+from pathlib import Path
+
+from plover_build_utils.testing import dictionary_test
 
 from plover_casecat_dictionary import CaseCatDictionary
 
-from plover import log, system
-from plover.config import DEFAULT_SYSTEM_NAME
-from plover.dictionary.base import load_dictionary
-from plover.formatting import Formatter
-from plover.registry import registry
-from plover.steno import normalize_steno, Stroke
-from plover.steno_dictionary import StenoDictionary
-from plover.translation import Translator
 
-log.set_level(log.DEBUG)
+@dictionary_test
+class TestCaseCatDictionary:
 
-DICTIONARIES = {
-        u'S-abc.sgdct': {
-            'mappings': {'S': u'abc'},
-            },
-        u'S-abc,T-abc.sgdct': {
-            'mappings': {'S': u'abc', 'T': u'abc'},
-            },
-        u'S-dis.sgdct': {
-            'mappings': {'S': u'{dis^}'},
-            },
-        u'S-ing.sgdct': {
-            'mappings': {'S': u'{^ing}'},
-            },
-        u'S_S-abc.sgdct': {
-            'mappings': {'S/S': u'abc'},
-            },
+    DICT_CLASS = CaseCatDictionary
+    DICT_EXTENSION = 'sgdct'
+    DICT_REGISTERED = True
+    DICT_LOAD_TESTS = (
+        lambda: (
+            'S-abc.sgdct',
+            '''
+            'S': 'abc',
+            '''
+        ),
+        lambda: (
+            'S-abc,T-abc.sgdct',
+            '''
+            'S': 'abc',
+            'T': 'abc',
+            '''
+        ),
+        lambda: (
+            'S-dis.sgdct',
+            '''
+            'S': '{dis^}',
+            '''
+        ),
+        lambda: (
+            'S-ing.sgdct',
+            '''
+            'S': '{^ing}',
+            '''
+        ),
+        # No test for "cap up", because we don't yet
+        # have a genuine file that contains it.
+    )
+    DICT_SAMPLE = 'S-abc.sgdct'
 
-        # No test for "cap up", because
-        # we don't yet have a genuine file
-        # that contains it
-
-        }
-
-class CaseCatDictionaryTest(unittest.TestCase):
-
-    @classmethod
-    def setUpClass(cls):
-        registry.update()
-        system.setup(DEFAULT_SYSTEM_NAME)
-
-    def _dictionaries_iterator(self):
-        for (filename, expected) in sorted(DICTIONARIES.items()):
-
-            dic = load_dictionary(os.path.join(os.path.dirname(__file__), filename))
-            yield (filename, expected, dic)
-
-    def test_readonly(self):
-
-        for (filename, expected, dic) in self._dictionaries_iterator():
-            self.assertTrue(dic.readonly)
-
-    def test_load_dictionary(self):
-
-        for (filename, expected, dic) in self._dictionaries_iterator():
-
-            for (k, v) in expected['mappings'].items():
-                self.assertEqual(dic[k], v)
-
-
+    @staticmethod
+    def make_dict(contents):
+        path = Path(__file__).parent / contents
+        return path.read_bytes()
